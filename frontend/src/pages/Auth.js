b5814c58e1889f8a7880109818e3e28eddf60252
outsider <787552171@qq.com>
2021-03-11 14:30:05 +0800

    #13 Hitting the API | Fixed BUG
    对应内容：#13 Hitting the API | Build a Complete App with GraphQL, Node.js, MongoDB and React.js
    
    主要内容：
    - 完成登录、注册表单，及其对应的逻辑
    - 由于涉及前后端的通信，在后端实现跨域的中间件【及其重要】
    - 修复了在后端的auth resolver中的一个bug

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,9 +1,95 @@
 import React, { Component } from 'react';
+import './Auth.css';
+
 
 class AuthPage extends Component  {
+    constructor (props) {
+        super(props);
+        this.emailEl = React.createRef();
+        this.passwordEl = React.createRef();
+
+        this.state = {
+            isLoginMode: true
+        }
+    }
+
+    submitHandler = async (event) => {
+        event.preventDefault();
+        let email = this.emailEl.current.value;
+        let password = this.passwordEl.current.value;
+        if (email.trim() === "" || password.trim() === "") {
+            return false;
+        }
+        const requestBody = {
+            query: `
+            mutation {
+                createUser(userInput: {email: "${email}", password: "${password}"}) {
+                    _id
+                    email
+                }
+            }
+            `
+        };
+
+        if (this.state.isLoginMode) {
+            requestBody.query = `
+                query {
+                    login(email: "${email}", password: "${password}") {
+                        _id
+                        token
+                        tokenExpiration
+                    }
+                }
+                `;
+        }
+        let response, data;
+        try {
+            response = await fetch(`http://localhost:3030/graphql`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(requestBody)
+            });
+
+            data = await response.json();
+        } catch (error) {
+            throw error;
+        }
+        console.log(response);
+        console.log(data);
+    }
+
+    swithHandler = () => {
+        this.setState(prevState => {
+            return {
+                isLoginMode: !prevState.isLoginMode
+            };
+        })
+    }
+    
+
     render () {
         return (
-            <h1>The Auth Page</h1>
+            <React.Fragment>
+                <h1>The Auth Page</h1>
+                <form action="" className="auth-form" onSubmit={ this.submitHandler }>
+                    <div className="form-control">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" ref={this.emailEl} />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" ref={this.passwordEl} />
+                    </div>
+                    <div className="form-action">
+                        <button type="submit">Submit</button>
+                        <button type="button" onClick={this.swithHandler}>
+                            swith to {this.state.isLoginMode ? "Signup" : "Signin"}
+                        </button>
+                    </div>
+                </form>
+            </React.Fragment>
         );
     }
 }
--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -5,21 +5,22 @@ const User = require("../../models/user");
 const rootValue = {
     createUser: async args => {
         try {
-            const user = User.findOne({ email: args.userInput.email })
+            console.log(args.userInput.email);
+            let user = await User.findOne({ email: args.userInput.email })
             if (user) {
+                console.log("user existed");
                 throw new Error("user already existed");
-            } else {
-                const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
-                const user = new User({
-                    email: args.userInput.email,
-                    password: hashedPassword,
-                })
-                const savedUser = await user.save();
-                return {
-                    ...savedUser._doc,
-                    // 为了安全起见，这里返回null去代替password
-                    password: null
-                }
+            }
+            const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+            user = new User({
+                email: args.userInput.email,
+                password: hashedPassword,
+            })
+            const savedUser = await user.save();
+            return {
+                ...savedUser._doc,
+                // 为了安全起见，这里返回null去代替password
+                password: null
             }
         } catch (error) {
             throw error;
